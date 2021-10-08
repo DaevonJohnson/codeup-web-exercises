f94ed3ca6180ab2c37dfbd4e4ea3f37eb687c893
daevon johnson <daevontjohnson01@gmail.com>
2021-10-08 16:45:15 -0500

    compelete the exercise walkthrough for js with html.

---
+++ js/external.js
@@ -0,0 +1,25 @@
+<script src="js/external.js"></script>
+var userInput = prompt('Hello from external JavaScript:');
+console.log('The user entered: ' + ' + userInput);
+var rentalPricePerDay =3
+var littlemermaid = prompt ("how many days did you rent the little mermaid Movie?");
+var brotherbear = prompt ("how many days did you rent the Brother Bear Movie");
+var hercules = prompt("how many days did you rent the hercules movie?");
+alert("your total rental cost was $" + new Intl.Numberformat().format(totalPrice) + ".");
+
+var google =400;
+var amazon =380;
+var facebook =350;
+var googlehours = prompt("how much time did you work for google this week");
+var amazonhours = prompt("how much time did you  work for amazon this weeek");
+var facebookhours = prompt (" how  much time did you work for facebook this week");
+payCheck = google * googlehours + amazon * amazonhours + facebook * facebookhours
+alert("Your paycheck this week will be $" + new Intl.Numberformat().formate (totalPrice) + ".");
+
+alert("Welcome to my Website");
+var userInput = prompt('What is your favorite color?:');
+console.log('The user entered: blue' + userInput);
+alert("Great, blue is my favorite color too");
+var userInput = prompt('Whats your favorite Movie?:');
+console.log('The user entered: avengers' + userInput);
+alert("marvel movies are great");
