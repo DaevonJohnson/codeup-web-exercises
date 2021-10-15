4e2fc9144b0750361bd62e4c0138c5a47c07740b
DaevonJohnson <91964680+DaevonJohnson@users.noreply.github.com>
2021-10-15 13:53:24 -0500

    Add files via upload

---
+++ js/solutions.js
@@ -0,0 +1,55 @@
+"use strict";
+
+/**
+ * Write your solutions here.
+ *
+ * **Note**: While normally it is good practice to wrap your javascript in an
+ * immediately invoked function expression (iife), you should _not_ do that
+ * here. If you do, the automated tests will not be able to see your functions.
+ * If you don't know what an iffe is, don't worry about it :)
+ *
+ * This file is for defining the following functions. See README.md for more detail.
+ * isTrue
+ * isFalse
+ * not
+ * addOne
+ * isEven
+ * isIdentical
+ * isEqual
+ * or
+ * and
+ * concat
+ */
+
+function  isTrue(input){
+    return input === true;
+
+}
+function  isFalse (input){
+    return input === false
+}
+function  not (input) {
+    return input === null
+}
+function  addOne (input) {
+    return input === false
+}
+function  isEven (input) {
+    return input === true
+}
+function  isIdentical(input) {
+    return input === true
+}
+function  isEqual(input) {
+    return input === true
+}
+function  or (input) {
+    return input === true
+}
+function  and (input) {
+    return input === null;
+}
+function  concat (input) {
+    return input === true
+    console.log ("hello", "world")
+}
