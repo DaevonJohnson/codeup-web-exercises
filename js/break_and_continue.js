56ff0d471ac503200f085e73f930f5c529d167b5
daevon johnson <daevontjohnson01@gmail.com>
2021-10-19 14:58:43 -0500

    compeleted

---
+++ js/break_and_continue.js
@@ -0,0 +1,41 @@
+"use strict";
+
+(function() {
+
+    var userNum
+
+    while (true){
+        userNum = parseInt(prompt("Give me an odd number between 1 and 50"));
+        if (userNum >= 1 && userNum<= 50 && userNum % 2 !== 0) {
+            break;
+        }
+    }
+
+    for (var b = 1; b <= 50; b++) {
+        if (b % 2 === 0) {
+            continue;
+        }
+        if(b % 2 === 0) {
+            console.log("we dont talk about" + userNum+ " around these parts...")
+        }
+    }
+})()
+
+var userNum = numberber (prompt( "enter an odd number between 1 and 50")
+for (var y = 1; Y < 50; i++) {
+    if (y ==== 1)
+console.log('number to skip is' + userNum)
+}
+
+if(y % 2=== 0) {
+    continue;
+}
+if (y === userNum) {
+    console.log ('skipping number' + userNum)
+} else {
+    console.log ('skipping number' + userNum)
+}else {
+    console.log ('here is an odd number' + y){
+
+    }
+}
---
+++ js/while.js
@@ -0,0 +1,24 @@
+"us strict";
+
+(funtion())
+
+var multipliedNum =2;
+
+while(multipliedNum <= 65536) {
+    multipliedNum*= 2;
+    console.log(multipliedNum);
+}
+var multipliedNum =1;
+
+while(multipliedNum <= 65536) {
+    multipliedNum*= 2;
+    console.log(multipliedNum);
+
+    var allcones = Math.floor (Math.random()* 5) + 50;
+    console.log("opening up shop ive got" + allcones + "cones to sell");
+    do {
+
+    var conesToBuy = Math.floor (math.random() * 5) + 1;
+    allcones _= conesToBuy
+        console.log("customer wants to buy" + conesToBuy +" cones. I have" + allcones +" cones left in stock" )
+    }
