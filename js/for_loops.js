618f61fbfcf14b860a4b726c0afdd97c0cf60a34
DaevonJohnson <91964680+DaevonJohnson@users.noreply.github.com>
2021-10-19 10:58:37 -0500

    Add files via upload

---
+++ js/for_loops.js
@@ -0,0 +1,32 @@
+function showMultiplicationTable(num) {
+
+    for (var i =1; i<= 10; i++){
+        var answer = i * num;
+        console.log (`${num} x ${i} = ${answer}` );
+
+
+
+    }
+}
+showMultiplicationTable(7);
+// for (var i = 0; i ,<=10; i++);
+
+// var randomNum = Math.floor (Math.random() * 200) + 20;
+//
+// if ( randomNum % 2 === 0) {
+//     console.log(randomNum + "is an even number")
+// }else{
+//     (randomNum % 2 === 0) {
+//         console.log(randomNum + " is an odd number")
+//     }
+//
+//     for (var k i = 1; i < 10; i++) {
+//     console.log(i.tostring().repeat(i));
+//     }
+// }
+
+// for (var k = 100; k >=5) {
+//     console.log(k);
+//
+// }
+//
