0edf8b7da9f1a52600141f213f7d50899e7fc79e
daevon johnson <daevontjohnson01@gmail.com>
2021-11-09 09:19:43 -0600

    commit

--- js/warmup.js
+++ js/warmup.js
@@ -30,5 +30,65 @@ var area =Math.PI * (Math.pow(circle.radius, 2))
 console.log(area);
 
 
-
-
+var groceries = [
+    {
+        name: "carrots",
+        quantity: 5
+    },{
+        name: "yams",
+        quantity: 50
+    },{
+        name: "oranges",
+        quantity: 9
+    },{
+        name: "onions",
+        quantity: 2
+    },{
+        name: "cucumbers",
+        quantity: 6
+    },{
+        name: "potatoes",
+        quantity: 8
+    }
+];
+var products = [
+    {
+        name: 'Playstation 5',
+        price: 599.99
+        return  Playstation 5
+    }, {
+        name: 'Logitech Wireless Mouse',
+        price: 23.99
+    }, {
+        name: 'Macbook Pro',
+        price: 1099.99
+    }, {
+        name: 'GoPro HERO10',
+        price: 399.99
+    }, {
+        name: '12" & 6" Metal Ruler Set',
+        price: 5.99
+    }]
+var bBallPlayers = [
+    {
+        name: "Hakeem Olajuwon",
+        height: 213
+    }, {
+        name: "Muggsy Bogues",
+        height: 160
+    }, {
+        name: "Chris Paul",
+        height: 183
+    }, {
+        name: "Bol Bol",
+        height: 218
+    }, {
+        name: "Moochie Norris",
+        height: 185
+    }, {
+        name: "Manu Ginobili",
+        height: 198
+    }
+];
+console.log(bBallPlayers)
+var bBallPlayers: [{name: "Hakeem Olajuwon" string, height: number}, {name:"Muggsy Bogues" string, height: number}, {name: "Chris Paul" string, height: number}, {name: string, height: number}, string, height: number}, {name
